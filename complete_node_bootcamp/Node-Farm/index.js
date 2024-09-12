9f281d7507cc567a6039b39e20c2674df8ee2406
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-09-12 09:14:20 -0500

    update

--- complete_node_bootcamp/Node-Farm/index.js
+++ complete_node_bootcamp/Node-Farm/index.js
@@ -37,6 +37,20 @@ console.log("Reading...")
  * SERVER
  ***************************/
 
+const replaceTemplate = (temp, product) => {
+	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+	output = output.replace(/{%IMAGE%}/g, product.image)
+	output = output.replace(/{%PRICE%}/g, product.price)
+	output = output.replace(/{%FROM%}/g, product.from)
+	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+	output = output.replace(/{%QUANTITY%}/g, product.quantity)
+	output = output.replace(/{%DESCRIPTION%}/g, product.description)
+
+	if (!product.organic)
+		output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic")
+	return output
+}
+
 // Read in json data
 const tempOverview = fs.readFileSync(
 	`${__dirname}/templates/template-overview.html`,
@@ -50,8 +64,9 @@ const tempProduct = fs.readFileSync(
 	`${__dirname}/templates/template-product.html`,
 	"utf-8"
 )
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
 
+// Read data.json file that contains all the product information
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
@@ -60,7 +75,15 @@ const server = http.createServer((req, res) => {
 	// OVERVIEW PAGE
 	if (pathName === "/" || pathName === "/overview") {
 		res.writeHead(200, { "Content-type": "text/html" })
-		res.end(tempOverview)
+
+		const cardsHtml = dataObj
+			.map((el) => replaceTemplate(tempCard, el))
+			.join("")
+
+		const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml)
+
+		res.end(output)
+
 		// PRODUCT PAGE
 	} else if (pathName === "/product") {
 		res.end("This is the product page!!")
