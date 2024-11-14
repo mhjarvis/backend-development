d4e2544424abd7d40be69db840d73e889457a4e6
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-11-13 18:13:41 -0600

    Setup server with redirection and basic post/get info

--- nodejs-complete-guide/01_basic_node_server/app.js
+++ nodejs-complete-guide/01_basic_node_server/app.js
@@ -1,16 +1,49 @@
 // require() - default way to import in node
 const http = require("http")
+const fs = require("fs") // allows access to file system
 
-/* function rqListener(req, res) {}
+const server = http.createServer((req, res) => {
+	//console.log(req.url, req.method, req.headers)
+	//process.exit()		this will exit the process and close connection
+
+	const url = req.url
+	const method = req.method
+
+	if (url === "/") {
+		res.write("<html>")
+		res.write("<head><title>Enter Info</title></head>")
+		res.write(
+			"<body><form action='/message' method='POST'><input type='text' name='messsage'><button type='submit'>Send</button></form></body>"
+		)
+		res.write("</html>")
+		return res.end()
+	}
+
+	if (url === "/message" && method === "POST") {
+		fs.writeFileSync("message.txt", "DUMMY")
+		res.statusCode = 302
+		res.setHeader("Location", "/")
+		return res.end()
+	}
+
+	res.setHeader("Content-Type", "text/html")
+	res.write("<html>")
+	res.write("<head><title>My first page</title></head>")
+	res.write("<body><h1>Hello there from the Node.js server</h1></body>")
+	res.write("</html>")
+	res.end()
+})
+
+// this will start the server to continually listen for incoming requests
+server.listen(3000)
+
+/* 
+function rqListener(req, res) {}
 
 // this will create a server that will run the called function for every request that it gets
-http.createServer(rqListener) */
+http.createServer(rqListener) 
+*/
 
-/* http.createServer(function (req, res) {
-
-})
+/* 
+http.createServer(function (req, res) {})
  */
-
-http.createServer((req, res) => {
-	console.log(req)
-})
