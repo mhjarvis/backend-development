317e504ebedece97f30bf913ccb657c23d4ec911
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-12-29 09:37:20 -0600

    finish up 01_basic_server

--- Node_Stuff/01_basic_server/app.js
+++ Node_Stuff/01_basic_server/app.js
@@ -1,21 +1,7 @@
 const http = require("http")
 
-function rqListener(req, res) {
-	// process.exit()
-	const url = req.url
+const routes = require("./routes")
 
-	if (url === "/") {
-		res.setHeader("Content-Type", "text/html")
-		res.write(
-			'<html><body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body></html>'
-		)
-		return res.end()
-	}
-	res.setHeader("Content-Type", "text/html")
-	res.write("<html><h1>Hello World</h1></html>")
-	res.end()
-}
-
-const server = http.createServer(rqListener)
+const server = http.createServer(routes)
 
 server.listen(3000) // listens for each incoming request
